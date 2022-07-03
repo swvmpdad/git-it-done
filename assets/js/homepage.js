30972a4237db3ef3b66d610a278236c89c5d2cec
swvmpdad <will.g.camp@gmail.com>
2022-07-03 16:10:06 -0500

    added featured repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,21 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('error: GitHub User Not Found');
+        }
+    });
+};
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -82,4 +97,15 @@ var getUserRepos = function(user) {
     });
 };
 
+var buttonClickHandler = function(event) {
+    event.preventDefault();
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
