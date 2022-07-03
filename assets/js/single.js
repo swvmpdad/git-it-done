0a44b324a1e95d63a3fd6c5efcdda4083bf87c8f
swvmpdad <will.g.camp@gmail.com>
2022-07-03 15:08:38 -0500

    added link to github for more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +10,22 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to DOM function
                 displayIssues(data);
+
+                // check if API has paginated issues
+                if (response.headers.get("Link")) {
+                    var displayWarning = function(repo) {
+                        // add text to warning container
+                        limitWarningEl.textContent = "To see more than 30 issues, visit ";
+                        var linkEl = document.createElement("a");
+                        linkEl.textContent = "See more issues on GitHub.com";
+                        linkEl.setAttribute = "href", "https://github.com/" + repo + "/issues";
+                        linkEl.setAttribute = "target", "_blank";
+
+                        // append to warning container
+                        limitWarningEl.appendChild(linkEl);
+                    }
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -50,4 +67,4 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("swvmpdad/quizzy");
+getRepoIssues("facebook/react");
